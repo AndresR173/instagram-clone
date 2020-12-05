4ae509b61419b68ac85975a47a623c3b6fc2a04a
Andres Acosta <Andres.Acosta@endava.com>
2020-12-05 13:53:35 -0500

    dividing Post widget into components, using Like widget

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -11,75 +11,89 @@ class Post extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.only(top: 0, bottom: 25),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.only(left: 15, right: 5),
-                        child: UserImage(
-                            image: publication.avatarImage,
-                            radio: 12,
-                            width: 30),
-                      ),
-                      Text(publication.accountName,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                    ],
-                  ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 15),
+      child: Column(children: <Widget>[
+        buildHeader(),
+        buildImage(),
+        buildActions(),
+        buildLikes()
+      ]),
+    );
+  }
+
+  Widget buildImage() {
+    return Padding(
+        padding: const EdgeInsets.only(top: 10, bottom: 10),
+        child: RandomImage(media: publication.media));
+  }
+
+  Widget buildHeader() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 8, left: 20),
+                child: UserImage(
+                    image: publication.avatarImage, radio: 16, width: 38),
+              ),
+              Text(publication.accountName,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16)),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 25),
+          child: Icon(Icons.more_vert, color: Colors.white),
+        )
+      ],
+    );
+  }
+
+  Widget buildActions() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 10, left: 20),
+                child: Icon(
+                  Icons.favorite_border,
+                  color: Colors.white,
+                  size: 35,
                 ),
-                Container(
-                  padding: EdgeInsets.only(right: 20),
-                  child: Icon(Icons.more_vert, color: Colors.white),
-                )
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              child: RandomImage(media: publication.media),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                    child: Container(
-                  padding: EdgeInsets.only(left: 10, top: 7, bottom: 7),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                          padding: EdgeInsets.only(right: 10),
-                          child: Icon(
-                            Icons.favorite_border,
-                            size: 30,
-                            color: Colors.white,
-                          )),
-                      Container(
-                          padding: EdgeInsets.only(right: 20),
-                          child: Icon(Icons.chat_bubble_outline,
-                              size: 30, color: Colors.white)),
-                      Container(
-                          padding: EdgeInsets.only(right: 20),
-                          child:
-                              Icon(Icons.send, size: 30, color: Colors.white)),
-                    ],
-                  ),
-                )),
-                Container(
-                    padding: EdgeInsets.only(right: 10),
-                    child: Icon(Icons.save, size: 30, color: Colors.white)),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Container(padding: EdgeInsets.only(left: 13), child: Likes())
-              ],
-            )
-          ],
-        ));
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Icon(Icons.chat_bubble_outline,
+                    color: Colors.white, size: 35),
+              ),
+              Icon(Icons.send, color: Colors.white, size: 35),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 25),
+          child: Icon(Icons.save, color: Colors.white, size: 35),
+        )
+      ],
+    );
+  }
+
+  Widget buildLikes() {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 5),
+          child: Likes(),
+        ),
+      ],
+    );
   }
 }
