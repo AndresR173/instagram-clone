2d89db3d854f1a063c550ddcb55391245d5a9a24
Andres Acosta <Andres.Acosta@endava.com>
2020-12-05 13:20:01 -0500

    dividing Post widget into components

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,4 +1,3 @@
-import 'package:fakestagram/widgets/likes.dart';
 import 'package:fakestagram/widgets/user_image.dart';
 import 'package:flutter/material.dart';
 
@@ -9,75 +8,94 @@ class Post extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.only(top: 0, bottom: 25),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.only(left: 15, right: 5),
-                        child: UserImage(
-                            image: "assets/images/post/avatar.jpg",
-                            radio: 12,
-                            width: 30),
-                      ),
-                      Text("Account $index",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                    ],
-                  ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 15),
+      child: Column(children: <Widget>[
+        buildHeader(),
+        buildImage(),
+        buildActions(),
+        buildLikes()
+      ]),
+    );
+  }
+
+  Widget buildImage() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: Image.asset('assets/images/post/post$index.jpg'),
+    );
+  }
+
+  Widget buildHeader() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 8, left: 20),
+                child: UserImage(
+                    image: 'assets/images/post/avatar.jpg',
+                    radio: 16,
+                    width: 38),
+              ),
+              Text('Account $index',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16)),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 25),
+          child: Icon(Icons.more_vert, color: Colors.white),
+        )
+      ],
+    );
+  }
+
+  Widget buildActions() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(right: 10, left: 20),
+                child: Icon(
+                  Icons.favorite_border,
+                  color: Colors.white,
+                  size: 35,
                 ),
-                Container(
-                  padding: EdgeInsets.only(right: 20),
-                  child: Icon(Icons.more_vert, color: Colors.white),
-                )
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 10),
-              child: Image.asset("assets/images/post/post$index.jpg"),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                    child: Container(
-                  padding: EdgeInsets.only(left: 10, top: 7, bottom: 7),
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                          padding: EdgeInsets.only(right: 10),
-                          child: Icon(
-                            Icons.favorite_border,
-                            size: 30,
-                            color: Colors.white,
-                          )),
-                      Container(
-                          padding: EdgeInsets.only(right: 20),
-                          child: Icon(Icons.chat_bubble_outline,
-                              size: 30, color: Colors.white)),
-                      Container(
-                          padding: EdgeInsets.only(right: 20),
-                          child:
-                              Icon(Icons.send, size: 30, color: Colors.white)),
-                    ],
-                  ),
-                )),
-                Container(
-                    padding: EdgeInsets.only(right: 10),
-                    child: Icon(Icons.save, size: 30, color: Colors.white)),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Container(padding: EdgeInsets.only(left: 13), child: Likes())
-              ],
-            )
-          ],
-        ));
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Icon(Icons.chat_bubble_outline,
+                    color: Colors.white, size: 35),
+              ),
+              Icon(Icons.send, color: Colors.white, size: 35),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 25),
+          child: Icon(Icons.save, color: Colors.white, size: 35),
+        )
+      ],
+    );
+  }
+
+  Widget buildLikes() {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 5),
+          child: Text(
+            '30 likes',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+      ],
+    );
   }
 }
